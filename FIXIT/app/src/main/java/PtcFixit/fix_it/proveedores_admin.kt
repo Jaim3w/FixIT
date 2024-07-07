379a271f25171f20a36756be0b70cf5d7fa53d85
chaichor <christianpro313@gmail.com>
2024-07-07 17:36:24 -0600

    Avance: creacion de activity carros, config del nav, includes, cambios en diseño de nav y header

--- FIXIT/app/src/main/java/PtcFixit/fix_it/proveedores_admin.kt
+++ FIXIT/app/src/main/java/PtcFixit/fix_it/proveedores_admin.kt
@@ -1,12 +1,17 @@
 package PtcFixit.fix_it
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class proveedores_admin : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,39 @@ class proveedores_admin : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupNavClickListeners()
+
+    }
+
+    private fun setupNavClickListeners() {
+        val navView = findViewById<View>(R.id.include_nav)
+
+        val imageView6 = navView.findViewById<ImageView>(R.id.imageView6)
+        val imageView8 = navView.findViewById<ImageView>(R.id.imageView8)
+        val imageView7 = navView.findViewById<ImageView>(R.id.imageView7)
+        val imageView9 = navView.findViewById<ImageView>(R.id.imageView9)
+        val imageView10 = navView.findViewById<ImageView>(R.id.imageView10)
+
+        val clickListener = View.OnClickListener { v ->
+            val intent = when (v.id) {
+                R.id.imageView6 -> Intent(this, MenuAdmin::class.java)
+                R.id.imageView8 -> Intent(this, Repuesto::class.java)
+                R.id.imageView7 -> Intent(this, proveedores_admin::class.java)
+                R.id.imageView9 -> Intent(this, carros_admin::class.java)
+                R.id.imageView10 -> Intent(this, citas::class.java)
+                else -> null
+            }
+            intent?.let { startActivity(it) }
+        }
+
+        imageView6.setOnClickListener(clickListener)
+        imageView8.setOnClickListener(clickListener)
+        imageView7.setOnClickListener(clickListener)
+        imageView9.setOnClickListener(clickListener)
+        imageView10.setOnClickListener(clickListener)
     }
 }
+
+
+
