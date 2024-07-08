7b43c8d30fc912a5ebcaa0f192c6bd05901f5a60
Jaime Ernesto Perla Romero <jaimeperla26@gmail.com>
2024-07-07 21:51:47 -0600

    cracion de un fragment

--- FIXIT/app/src/main/java/PtcFixit/fix_it/citas.kt
+++ FIXIT/app/src/main/java/PtcFixit/fix_it/citas.kt
@@ -1,10 +1,12 @@
 package PtcFixit.fix_it
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class citas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,26 @@ class citas : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val txtCrearCita = findViewById<TextView>(R.id.txtcrearCita)
+        val txtCitaspendientes = findViewById<TextView>(R.id.txtCitasPendientes)
+
+        replaceFragment(citasFragment())
+
+        txtCrearCita.setOnClickListener {
+            replaceFragment(citasFragment())
+        }
+
+        txtCitaspendientes.setOnClickListener {
+            replaceFragment(citas_fragment2())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
+
+
