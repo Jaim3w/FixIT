4f909e332ddd677b71633f3b668d38ef45c13251
Adriel <20230542@ricaldone.edu.sv>
2024-07-13 18:57:29 -0600

    pulleando cambios que hice en otra compu

--- FIXIT/app/src/main/java/Modelo/ClaseConexion.kt
+++ FIXIT/app/src/main/java/Modelo/ClaseConexion.kt
@@ -7,7 +7,7 @@ import java.sql.DriverManager
 class ClaseConexion {
     fun cadenaConexion():Connection?{
         try{
-            val ip="jdbc:oracle:thin:@192.168.1.6:1521:xe"
+            val ip="jdbc:oracle:thin:@192.168.0.11:1521:xe"
             val usuario= "fito_DEVELOPER"
             val contrasena= "qatar2024"
             val conexion=DriverManager.getConnection(ip, usuario , contrasena)
