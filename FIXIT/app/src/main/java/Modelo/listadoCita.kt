79536bd22031c39c0313fc6c2c5aa1ba8b69eb8d
chaichor <christianpro313@gmail.com>
2024-07-14 10:20:08 -0600

    inicio de mostrar datos en menu

---
+++ FIXIT/app/src/main/java/Modelo/listadoCita.kt
@@ -0,0 +1,11 @@
+package Modelo
+
+data class listadoCita(
+
+    var UUID_cita : String,
+    var Dui_cliente : String,
+    var Dui_Empleado : String,
+    var Fecha_cita : String,
+    var Descripcion : String
+
+)
