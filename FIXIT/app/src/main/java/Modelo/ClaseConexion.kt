12d1a98649dd04907c3cf3e5bde9d5f1263439ea
Jaime Ernesto Perla Romero <jaimeperla26@gmail.com>
2024-07-13 21:22:55 -0600

    u qmal

--- FIXIT/app/src/main/java/Modelo/ClaseConexion.kt
+++ FIXIT/app/src/main/java/Modelo/ClaseConexion.kt
@@ -7,7 +7,8 @@ import java.sql.DriverManager
 class ClaseConexion {
     fun cadenaConexion():Connection?{
         try{
-            val ip="jdbc:oracle:thin:@192.168.56.1:1521:xe"
+            
+            val ip="jdbc:oracle:thin:@172.18.224.1:1521:xe"
             val usuario= "Josue_Developper"
             val contrasena= "josue1234"
             val conexion=DriverManager.getConnection(ip, usuario , contrasena)
