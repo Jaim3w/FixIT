4784c33258da1b1ee6c4724c20c74ea14502ea09
Jaime Ernesto Perla Romero <jaimeperla26@gmail.com>
2024-07-14 21:38:02 -0600

    cambios

--- FIXIT/app/src/main/java/PtcFixit/fix_it/citasFragment.kt
+++ FIXIT/app/src/main/java/PtcFixit/fix_it/citasFragment.kt
@@ -97,7 +97,7 @@ class citasFragment : Fragment() {
         return listadoClientes
     }
 
-    private fun getEmpleados(): List<dataClassEmpleados>{
+    fun getEmpleados(): List<dataClassEmpleados>{
 
         val conexion = ClaseConexion().cadenaConexion()
         val statement = conexion?.createStatement()
@@ -165,7 +165,7 @@ class citasFragment : Fragment() {
 
             withContext(Dispatchers.Main) {
                 val clienteAdapter =
-                ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, nombreCliente)
+                    ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, nombreCliente)
                 txtClienteCita.adapter = clienteAdapter
 
                 val empleadoAdapter =
@@ -257,31 +257,31 @@ class citasFragment : Fragment() {
                     }
                 }
 
-                }
-
             }
+
+        }
         return root
     }
-        companion object {
-            /**
-             * Use this factory method to create a new instance of
-             * this fragment using the provided parameters.
-             *
-             * @param param1 Parameter 1.
-             * @param param2 Parameter 2.
-             * @return A new instance of fragment citasFragment.
-             */
-            // TODO: Rename and change types and number of parameters
-            @JvmStatic
-            fun newInstance(param1: String, param2: String) =
-                citasFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment citasFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            citasFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
-        }
-
+            }
     }
+
+}
 
 
