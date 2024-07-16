8ef54b81bd71c0c332b6c280dc778223d70cf845
Rodolfo Perez <20230300@ricaldone.edu.sv>
2024-07-15 21:51:42 -0600

    d

--- FIXIT/app/src/main/java/Modelo/ClaseConexion.kt
+++ FIXIT/app/src/main/java/Modelo/ClaseConexion.kt
@@ -4,13 +4,14 @@ package Modelo
 import java.sql.Connection
 import java.sql.DriverManager
 
-class ClaseConexion {
+class  ClaseConexion {
     fun cadenaConexion():Connection?{
         try{
-            val url="jdbc:oracle:thin:@192.168.1.6:1521:xe"
-            val usuario= "fito_DEVE"
-            val contrasena= "fito17"
-            val conexion=DriverManager.getConnection(url, usuario , contrasena)
+            
+            val ip="jdbc:oracle:thin:@172.18.224.1:1521:xe"
+            val usuario= "lacobraa"
+            val contrasena= "qatar24"
+            val conexion=DriverManager.getConnection(ip, usuario , contrasena)
             return conexion
         }catch (e:Exception){
             println("El error es este $e")
